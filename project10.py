7c53ada7893763ac93324ae611808637196160bf
R. Hannes Niedner <hannes.niedner@ucsf.edu>
2014-12-08 07:22:22 -0800

    project10 add timing, output format

--- project10.py
+++ project10.py
@@ -5,6 +5,10 @@
 # Find the sum of all the primes below two million.
 __author__ = 'hannes'
 import math
+import time
+
+startTime = time.clock()
+print "Calculating..."
 
 
 def calc_prime(n):  # calculates range (largest) not nth prime
@@ -16,7 +20,8 @@ def calc_prime(n):  # calculates range (largest) not nth prime
     return [i for i in xrange(len(a)) if a[i] == 1][2:]
 
 
-print reduce(lambda x, y: x + y, calc_prime(2000000))
+print "sum is ->> " + str(reduce(lambda x, y: x + y, calc_prime(2000000)))
+print "My Program took %.4f secs to execute\n" % (time.clock() - startTime)
 
 
 # You are the 164744th person to have solved this problem.
